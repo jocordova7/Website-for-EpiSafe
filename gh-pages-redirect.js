457176f84c6cc6f9128289c91e6eb10fce8f66bb
Jaime Ortiz <jcordova@wpi.edu>
2025-03-19 11:35:46 +0100

    Updates

--- gh-pages-redirect.js
+++
@@ -1,4 +0,0 @@
-
-// This hack ensures GitHub Pages correctly serves the index.html at the root
-// and not the README from the main branch
-window.location.href = window.location.pathname.replace(/\/$/, '') + '/index.html';
